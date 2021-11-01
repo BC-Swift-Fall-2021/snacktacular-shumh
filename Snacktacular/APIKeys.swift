d892476735c850b8a97cb04c521ec51fa7f7d3d2
Richard Shum <richard18shum@gmail.com>
2021-11-01 11:20:39 -0400

    8.7

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Richard Shum on 11/1/21.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyDWYjDRC1B4AngjatMW7GvgDkDqP_o8dcY"
+}
