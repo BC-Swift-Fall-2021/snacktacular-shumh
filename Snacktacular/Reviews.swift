0895559b7cd746748cdc98f1fc820edfebcfea48
Richard Shum <richard18shum@gmail.com>
2021-11-08 15:34:32 -0500

    8.15

--- Snacktacular/Reviews.swift
+++ Snacktacular/Reviews.swift
@@ -16,4 +16,24 @@ class Reviews {
         db = Firestore.firestore()
         
     }
+    
+    func loadData(spot:Spot, completed: @escaping () -> ()) {
+        guard spot.documentID != "" else {
+            return
+            
+        }
+        db.collection("spots").document(spot.documentID).collection("reviews").addSnapshotListener { (QuerySnapshot, error) in
+            guard error == nil else {
+                print("ERROR: adding the snapshot listener \(error!.localizedDescription)")
+                return completed()
+            }
+            self.reviewArray = []
+            for document in QuerySnapshot!.documents {
+                let review = Review(dictionary: document.data())
+                review.documentID = document.documentID
+                self.reviewArray.append(review)
+            }
+            completed()
+        }
+    }
 }
