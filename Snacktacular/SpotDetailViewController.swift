aa5121ea8dc381545151ced98bff252875427d24
Richard Shum <richard18shum@gmail.com>
2021-11-14 11:47:27 -0500

    8.20

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -82,6 +82,16 @@ class SpotDetailViewController: UIViewController {
             let selectedIndexPath = tableView.indexPathForSelectedRow!
             destination.review = reviews.reviewArray[selectedIndexPath.row]
             destination.spot = spot
+        case "AddPhoto":
+            let navigationControler = segue.destination as! UINavigationController
+            let destination = navigationControler.viewControllers.first as! PhotoViewController
+            destination.spot = spot
+            
+        case "ShowPhoto":
+            let destination = segue.destination as! PhotoViewController
+//            let selectedIndexPath = tableView.indexPathForSelectedRow!
+//            destination.review = reviews.reviewArray[selectedIndexPath.row]
+            destination.spot = spot
         default:
             print("Couldn't find a case for segue identifier \(segue.identifier). This should not have happened!")
             
@@ -179,6 +189,14 @@ class SpotDetailViewController: UIViewController {
             saveCancelAlert(title: "This Venue Has Not Been Saved", message: "You must save this venue before you can review it", segueIdentifier: "AddReview")
         } else {
             performSegue(withIdentifier: "AddReview", sender: nil)
+        }
+    }
+    
+    @IBAction func photoButtonPressed(_ sender: UIButton) {
+        if spot.documentID == "" {
+            saveCancelAlert(title: "This Venue Has Not Been Saved", message: "You must save this venue before you can review it", segueIdentifier: "AddPhoto")
+        } else {
+            performSegue(withIdentifier: "AddPhoto", sender: nil)
         }
     }
     
